1de7381ccea80a3721b96125e5148eeb5f5bea72
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-09-27 22:35:07 -0400

    Comments and changes

--- pipe/compare_models.R
+++ pipe/compare_models.R
@@ -19,9 +19,17 @@ auc_tab <- data.frame(lda_auc = lda_auc@y.values,
 colnames(auc_tab) <- c("lda_auc", "qda_auc")
 auc_tab
 
+## We should work on being more tidy, e.g., can this be a ggplot?
+## In the meantime, I made colors that I can see clearly
+## and blew things up … need to fix the legend (unless we tidy)
+## Don't use png or pdf commands unless you need to; this can be handled 
+## automatically by the make pipeline (I'll show you)
+
 # Save ROC curve
-png("compare_models.png")
-plot(lda_pred_per, main = "ROC", col = 2)
-plot(qda_pred_per, main = "ROC", col = 3, add = TRUE)
-legend(0.6, 0.6, c("LDA", "QDA"), 2:3)
-dev.off()
+plot(lda_pred_per, main = "ROC"
+	, col = 2
+	, xlim = c(0, 0.1)
+	, ylim = c(0.9, 1)
+)
+plot(qda_pred_per, main = "ROC", col = 4, add = TRUE)
+legend(0.6, 0.6, c("LDA", "QDA"), c(2, 4))
