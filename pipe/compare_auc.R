3a7f7fbfc18ee1d0ee1300cc4cd1b071298a7bd0
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-10-05 10:41:35 -0400

    Meeting with Steve

--- pipe/compare_auc.R
+++ pipe/compare_auc.R
@@ -2,24 +2,22 @@ library(corrplot)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+theme_set(theme_bw())
 
-# This script loads available auc objects from the fitted models and summarize/plot them
-
+# Load available auc objects from the fitted models
 l <- ls()
 auc_list <- list()
-prop_malignant_list <- list()
-auc_objects <- grep("auc_result", l, value = T)
-prop_malignant_objects <- grep("malignant_result", l, value = T)
-for (i in 1:length(auc_objects)){
-	auc_list[[auc_objects[i]]] <- get(auc_objects[i])
-	prop_malignant_list[[prop_malignant_objects[i]]] <- get(prop_malignant_objects[i])
+prop_mal_list <- list()
+auc_obj <- grep("auc_result", l, value = TRUE)
+prop_mal_obj <- grep("malignant_result", l, value = TRUE)
+for (i in 1:length(auc_obj)){
+	auc_list[[auc_obj[[i]]]] <- get(auc_obj[[i]])
+	prop_mal_list[[prop_mal_obj[[i]]]] <- get(prop_mal_obj[[i]])
 }
 
-## AUC
-# Correlation between estimates
+# Correlation between estimates across partitions
 auc_df <- as.data.frame(auc_list)
-new_var_names <- gsub("*_auc\\w+", "", names(auc_df))
-names(auc_df) <- new_var_names
+names(auc_df) <- gsub("*_auc\\w+", "", names(auc_df))
 corr <- cor(auc_df, method = "spearman")
 corrplot(corr, method="number", bg = "gray88")
 # Create the partition set variable
@@ -27,25 +25,12 @@ auc_df <- auc_df %>%
 	mutate(partition_set = paste("set", 1:n())) %>%
 	gather(model, auc_value, -partition_set)
 
-ggplot(data=auc_df, aes(x=partition_set, y=auc_value, group=model, colour=model)) +
-    geom_line() + 
-	 geom_point() + 
-	 xlab("Partition sets") + ylab("AUC") + 
-	 ggtitle("AUC scores across various sets of data partitions")
+print(
+	ggplot(data=auc_df, 
+		aes(x=model, y=auc_value, colour=model)
+	) 
+	+ geom_boxplot()
+	+ ylab("AUC")
+	## + ggtitle("AUC scores across various sets of data partitions")
+)
 
-## Proportion of predicted Malignant 
-# Correlation between estimates
-prop_malignant_df <- as.data.frame(prop_malignant_list)
-names(prop_malignant_df) <- new_var_names
-corr <- cor(prop_malignant_df, method = "spearman")
-corrplot(corr, method="number", bg = "gray88")
-# Create the partition set variable
-prop_malignant_df <- prop_malignant_df %>% 
-	mutate(partition_set = paste("set", 1:n())) %>%
-	gather(model, prop_malignant_value, -partition_set)
-
-ggplot(data=prop_malignant_df, aes(x=partition_set, y=prop_malignant_value, group=model, colour=model)) +
-    geom_line() + 
-	 geom_point() + 
-	 xlab("Partition sets") + ylab("Proportion of Predicted Malignant") + 
-	 ggtitle("Malignant proportions across various sets of data partitions")
