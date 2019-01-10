653064dc9bb9a14e4ed67fa198ee3ec90c507cac
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-01-10 12:21:23 -0500

    Working on presentation and scaling

---
+++ Presentation/tanh.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+
+library(ggplot2)
+theme_set(theme_bw())
+
+set.seed(237)
+
+# Function
+
+inscale <- 1/2
+inbias <- 0
+outscale <- 1/2
+outbias <- 1/2
+
+x <- seq(-3, 3, 0.1)
+df <- (data.frame(x = x)
+	%>% mutate(
+		xs = inscale*x + inbias
+		, Sigmoid = sigmoid(x)
+		, Tanh = 2*sigmoid(2*xs) - 1
+		, tscale = outscale*Tanh+outbias
+	)
+	%>% gather(Act_Func, Value, c(Sigmoid, tscale))
+)
+
+print(
+  ggplot(df, aes(x = x, y = Value, group = Act_Func, color = Act_Func)
+         )
+  + geom_line()
+  + labs(color = "Activation Function"
+         , title = "NN Activation functions"
+         , x = "x"
+         , y = "a(x)"
+         )
+  + theme(legend.position = c(0.2, .9),
+          plot.title = element_text(hjust = 0.5)
+          )
+)
